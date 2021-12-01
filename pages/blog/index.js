941acb7e9c8317402b2e5ebc1e9779893b90986c
dsmith <davidptsmith@gmail.com>
2021-12-01 16:43:05 +0800

    added logo and blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,34 +1,71 @@
-import Container from '../../components/blog/container'
-import MoreStories from '../../components/blog/more-stories'
-import { getAllPosts } from '../../lib/api'
+
+import Head from 'next/head'
+import BlogPresenter from '../../components/blog-2/BlogPresenter'
+
+import Link from 'next/link'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
 import NavPlus from '../../components/navplus';
 import PageHeader from '../../components/pageheader';
 
-export default function Index({ allPosts }) {
+
+const Blog = ({ posts }) => {
   return (
-    <>
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Data science with Daniel Blog page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <NavPlus/>
-    <PageHeader pageTitle="Blog"/>
+      <NavPlus />
 
-        <Container>
-          {allPosts.length > 0 && <MoreStories posts={allPosts} />}
-        </Container>
-    </>
+      <div className="mx-auto  mt-24 ">
+        <h2 className="text-4xl text-center">
+          Blog
+        </h2>
+      </div>
+
+      <div className="w-10/12 mx-auto my-24">
+        <BlogPresenter postObjs={posts} />
+      </div>
+
+    </div>
   )
 }
 
+export default Blog
+
+
 export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-  ])
+
+  //get files from post dir 
+  const files = fs.readdirSync(path.join('_posts'))
+
+  //get slug and front matter from posts 
+  const posts = files.map(filename => {
+
+    //create slug
+    const slug = filename.replace('.md', "")
+
+    const markdownWithMeta = fs.readFileSync(path.join('_posts', filename,), 'utf-8')
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
 
   return {
-    props: { allPosts },
+    props: {
+      //   posts: posts.sort(sortByDate),
+      posts: posts
+    }
   }
 }
+
