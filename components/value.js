e26f1b5675f5b4c0fd1bdd8b2ab0a13a0acaf832
arios92 <arioschean@hotmail.com>
2021-12-01 13:54:37 +0800

    remove aboutText

--- components/value.js
+++ components/value.js
@@ -7,7 +7,7 @@ const Value = (props) => {
   	return (<>
 		<Container>
            <Row>
-			   <Col className={styles.aboutText}>We seek to build a community of Data Scientists, so that we can share our passion and 
+			   <Col>We seek to build a community of Data Scientists, so that we can share our passion and 
 			   learn together. <br></br>We do this by bringing everything and everyone together in one place;
 			   Data Science with Daniel.
 			   </Col>
