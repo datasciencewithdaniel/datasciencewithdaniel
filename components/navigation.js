1f409230b5b78a4007760153b315c9d3010f2fd5
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2023-11-18 09:17:39 +0800

    add chop page

--- components/navigation.js
+++ components/navigation.js
@@ -1,29 +1,34 @@
 import { Nav, Navbar } from 'react-bootstrap';
 
 const Navigation = () => {
+  return (
+    <>
+      <Navbar expand="lg" variant="dark">
+        <Navbar.Brand href="/" className="pl-5">
+          <img
+            src="/logo/NewLogo_White_Small_Transparent.png"
+            alt="White Logo"
+            width="50"
+            height="50"
+          />{' '}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">
+              <span className="font-bold blueText">
+                Data Science with Daniel
+              </span>
+            </Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/blog">Blog</Nav.Link>
+            <Nav.Link href="/chop">Chop</Nav.Link>
+            {/* <Nav.Link href="/index/#contact">Contact</Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
 
-  	return (<>
-        <Navbar expand="lg" variant="dark">
-            <Navbar.Brand href="/" className="pl-5">
-                <img
-                    src="/logo/NewLogo_White_Small_Transparent.png"
-                    alt="White Logo" 
-                    width="50"
-                    height="50"
-                />{' '}
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/"><span className="font-bold blueText">Data Science with Daniel</span></Nav.Link>
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/blog">Blog</Nav.Link>
-                    <Nav.Link href="/tutoring">Tutoring</Nav.Link>
-                    {/* <Nav.Link href="/index/#contact">Contact</Nav.Link> */}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-	</>)
-}
-
-export default Navigation
+export default Navigation;
